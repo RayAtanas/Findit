f50be1e8c5923556aad83147ff4de318caf8734f
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2024-01-20 14:50:35 +0200

    init project

---
+++ findit/src/test/java/finditspring/findit/FinditApplicationTests.java
@@ -0,0 +1,13 @@
+package finditspring.findit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
