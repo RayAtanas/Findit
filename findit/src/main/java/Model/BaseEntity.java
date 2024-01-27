e625ba1b909702b10c3640fd37bad03ca0c203df
RayAtanas <rayatanas@hotmail.com>
2024-01-27 23:04:30 +0200

    initialized the project

--- findit/src/main/java/Model/BaseEntity.java
+++ findit/src/main/java/Model/BaseEntity.java
@@ -1,4 +1,14 @@
 package Model;
 
+import java.time.LocalDate;
+
 public class BaseEntity {
+
+
+    String Id ;
+    LocalDate Created_at;
+    LocalDate Updated_at;
+
+
+
 }
--- findit/src/main/java/finditspring/findit/FinditApplication.java
+++ findit/src/main/java/finditspring/findit/FinditApplication.java
@@ -2,12 +2,14 @@ package finditspring.findit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"Controller", "Model", "Service"})
+@EnableMongoRepositories(basePackages = {"Repository"})
 public class FinditApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FinditApplication.class, args);
 	}
-
 }
