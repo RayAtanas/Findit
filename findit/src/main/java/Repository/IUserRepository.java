9214f5ab2c11a934c196a3e8035d7c45f4080eb7
RayAtanas <rayatanas@hotmail.com>
2024-01-29 00:44:46 +0200

    added the findbyemail query

--- findit/src/main/java/Repository/IUserRepository.java
+++ findit/src/main/java/Repository/IUserRepository.java
@@ -3,7 +3,11 @@ package Repository;
 import Model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends MongoRepository<User, String> {
-    // Your repository methods here
+
+    Optional<User> findByEmail(String email);
 }
