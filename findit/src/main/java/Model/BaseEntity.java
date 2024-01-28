828711ee274b90fe7e6abba3f433677cd9529bab
RayAtanas <rayatanas@hotmail.com>
2024-01-29 00:44:14 +0200

    changed the id to uuid

--- findit/src/main/java/Model/BaseEntity.java
+++ findit/src/main/java/Model/BaseEntity.java
@@ -1,11 +1,14 @@
 package Model;
 
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class BaseEntity {
 
-
-    String Id ;
+    @Id
+    String id = UUID.randomUUID().toString();;
     LocalDate Created_at;
     LocalDate Updated_at;
 
