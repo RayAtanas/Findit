62ca1cf2b97a2be4ead06397241ceffe14d7b7ef
RayAtanas <rayatanas@hotmail.com>
2024-02-04 19:22:54 +0200

    created the Security config file for the password hashing

---
+++ findit/src/main/java/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,13 @@
+package SecurityConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends BCryptPasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
